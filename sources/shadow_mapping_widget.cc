9043f3cc38fce84fe65a0c8699f57e73858c400c
tatsy <tatsy.mail@gmail.com>
2015-07-17 18:26:06 +0900

    Under construction...

--- sources/shadow_mapping_widget.cc
+++ sources/shadow_mapping_widget.cc
@@ -1,20 +1,45 @@
+#include <cstdlib>
+#include <iostream>
+
 #include "shadow_mapping_widget.h"
 
 ShadowMappingWidget::ShadowMappingWidget(QWidget* parent)
     : QOpenGLWidget(parent)
+    , renderShader(NULL)
+    , shadowmapShader(NULL)
 {
 }
 
 ShadowMappingWidget::~ShadowMappingWidget()
 {
+    delete renderShader;
+    delete shadowmapShader;
 }
 
 void ShadowMappingWidget::initializeGL() {
+
 }
 
 void ShadowMappingWidget::paintGL() {
 }
 
 void ShadowMappingWidget::resizeGL(int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+QOpenGLShaderProgram* ShadowMappingWidget::compileShader(const QString& vShaderFile, const QString& fShaderFile) {
+    QOpenGLShaderProgram* shader = new QOpenGLShaderProgram();
+    shader->addShaderFromSourceFile(QOpenGLShader::Vertex, vShaderFile);
+    shader->addShaderFromSourceFile(QOpenGLShader::Fragment, fShaderFile);
+    shader->link();
+
+    if (!shader->isLinked()) {
+        std::cerr << "Failed to link shaders: " << std::endl;
+        std::cerr << "  Vertex: " << vShaderFile.toStdString() << std::endl;
+        std::cerr << "Fragment: " << fShaderFile.toStdString() << std::endl;
+        exit(1);
+    }
+
+    return shader;
 }
 
--- sources/shadow_mapping_widget.h
+++ sources/shadow_mapping_widget.h
@@ -4,7 +4,16 @@
 #include <QtWidgets/qwidget.h>
 #include <QtWidgets/qopenglwidget.h>
 
+#include <QtGui/qopenglshaderprogram.h>
+
 class ShadowMappingWidget : public QOpenGLWidget {
+    Q_OBJECT
+
+private:
+    static const int SHADOW_MAP_SIZE = 1024;
+    QOpenGLShaderProgram* renderShader;
+    QOpenGLShaderProgram* shadowmapShader;
+
 public:
     explicit ShadowMappingWidget(QWidget* parent = NULL);
     ~ShadowMappingWidget();
@@ -13,6 +22,9 @@ protected:
     void initializeGL() override;
     void paintGL() override;
     void resizeGL(int width, int height) override;
+
+private:
+    static QOpenGLShaderProgram* compileShader(const QString& vShaderFile, const QString& fShaderFile);
 };
 
 #endif // _SHADOW_MAPPING_WIDGET_H_
